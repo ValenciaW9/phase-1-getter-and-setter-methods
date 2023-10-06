d06706234bac341682524ef33b1caea98a8b8249
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-06 09:09:48 -0400

    phase-1-getter-and-setter-methods/
    phase-1-getter-and-setter-methods/

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-require ( './helpers.js' );
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
 
-let circle;
+  get diameter() {
+    return 2 * this.radius;
+  }
 
-describe( "Circle", () => {
-  beforeEach( () => {
-    circle = new Circle( 6 )
-  } )
+  set diameter(value) {
+    this.radius = value / 2;
+  }
 
-  it( "correctly returns the radius property of a Circle instance using `circle.radius`", () => {
-    expect( circle.radius ).to.equal( 6 )
-  } )
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
 
-  describe( "gets", () => {
-    it( "the diameter of a circle using the pseudo-property `circle.diameter`", () => {
-      expect( circle.diameter ).to.equal( 12 )
-    } )
-    it( "the circumference of a circle using the pseudo-property `circle.circumference`", () => {
-      expect( circle.circumference ).to.be.at.least( 37.68 )
-      expect( circle.circumference ).to.be.at.most( 37.71 )
-    } )
-    it( "the area of a circle using the pseudo-property `circle.area`", () => {
-      expect( circle.area ).to.be.at.least( 113 )
-      expect( circle.area ).to.be.at.most( 113.1 )
-    } )
-  } )
+  set circumference(value) {
+    this.radius = value / (2 * Math.PI);
+  }
 
-  describe( "sets", () => {
-    it( "the radius using the pseudo-property `circle.diameter`", () => {
-      circle.diameter = 36
-      expect( circle.radius ).to.eq( 18 )
-    } )
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
 
-    it( "the radius using the pseudo-property `circle.circumference`", () => {
-      circle.circumference = 100
-      expect( circle.radius ).to.be.at.least( 15 )
-      expect( circle.radius ).to.be.at.most( 16 )
-    } )
-
-    it( "the radius using the pseudo-property `circle.area`", () => {
-      circle.area = 200
-      expect( circle.radius ).to.be.at.least( 7 )
-      expect( circle.radius ).to.be.at.most( 8 )
-    } )
-  } )
-
-} )
+  set area(value) {
+    this.radius = Math.sqrt(value / Math.PI);
+  }
+}
