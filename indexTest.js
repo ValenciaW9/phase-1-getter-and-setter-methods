5fd32d36cf44c98938856e368c237a0aa2640cd0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-05 23:31:34 -0400

    phase-1-getter-and-setter-methods/
    phase-1-getter-and-setter-methods/

--- indexTest.js
+++ indexTest.js
@@ -1,31 +1,47 @@
-class Circle {
-  constructor(radius) {
-    this.radius = radius;
-  }
+require ( './helpers.js' );
 
-  get diameter() {
-    return this.radius * 2;
-  }
+let circle;
 
-  get circumference() {
-    return Math.PI * this.diameter;
-  }
+describe( "Circle", () => {
+  beforeEach( () => {
+    circle = new Circle( 6 )
+  } )
 
-  get area() {
-    return Math.PI * Math.pow(this.radius, 2);
-  }
+  it( "correctly returns the radius property of a Circle instance using `circle.radius`", () => {
+    expect( circle.radius ).to.equal( 6 )
+  } )
 
-  set diameter(value) {
-    this.radius = value / 2;
-  }
+  describe( "gets", () => {
+    it( "the diameter of a circle using the pseudo-property `circle.diameter`", () => {
+      expect( circle.diameter ).to.equal( 12 )
+    } )
+    it( "the circumference of a circle using the pseudo-property `circle.circumference`", () => {
+      expect( circle.circumference ).to.be.at.least( 37.68 )
+      expect( circle.circumference ).to.be.at.most( 37.71 )
+    } )
+    it( "the area of a circle using the pseudo-property `circle.area`", () => {
+      expect( circle.area ).to.be.at.least( 113 )
+      expect( circle.area ).to.be.at.most( 113.1 )
+    } )
+  } )
 
-  set circumference(value) {
-    this.radius = value / (2 * Math.PI);
-  }
+  describe( "sets", () => {
+    it( "the radius using the pseudo-property `circle.diameter`", () => {
+      circle.diameter = 36
+      expect( circle.radius ).to.eq( 18 )
+    } )
 
-  set area(value) {
-    this.radius = Math.sqrt(value / Math.PI);
-  }
-}
+    it( "the radius using the pseudo-property `circle.circumference`", () => {
+      circle.circumference = 100
+      expect( circle.radius ).to.be.at.least( 15 )
+      expect( circle.radius ).to.be.at.most( 16 )
+    } )
 
-module.exports = Circle;
+    it( "the radius using the pseudo-property `circle.area`", () => {
+      circle.area = 200
+      expect( circle.radius ).to.be.at.least( 7 )
+      expect( circle.radius ).to.be.at.most( 8 )
+    } )
+  } )
+
+} )
