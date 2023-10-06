7731fe2b5f0b6bcb01b197bf5a2cb6de17992cd0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-06 00:44:10 -0400

    phase-1-getter-and-setter-methods/
    phase-1-getter-and-setter-methods/

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-// Example event listener for a circle calculation button
-const circleButton = document.getElementById('circle-calculate-button');
-circleButton.addEventListener('click', () => {
-  const radius = parseFloat(document.getElementById('circle-radius').value);
-  const result = calculateCircleArea(radius);
-  document.getElementById('circle-result').textContent = `The area of the circle is: ${result}`;
-});
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  // Getter method for diameter
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  // Getter method for circumference
+  get circumference() {
+    return Math.PI * this.diameter;
+  }
+
+  // Getter method for area
+  get area() {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+
+  // Setter method for diameter
+  set diameter(newDiameter) {
+    this.radius = newDiameter / 2;
+  }
+
+  // Setter method for circumference
+  set circumference(newCircumference) {
+    this.radius = newCircumference / (2 * Math.PI);
+  }
+
+  // Setter method for area
+  set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI);
+  }
+}
